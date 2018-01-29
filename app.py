65f9ab58997c55e5cdff0cebfee91eccffb5990a
Priyansh Balyan <priyanshbalyan@gmail.com>
2018-01-29 20:08:01 +0530

    flask edit

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def handle_file():
 	return jsonify(message="ack")
 
 if __name__ == '__main__':
-	app.run(debug = True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
